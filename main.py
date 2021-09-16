c5942c229c0362fa63bae052d35402f85b805470
feshovets <90150282+feshovets@users.noreply.github.com>
2021-09-16 17:13:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 import math
 
-k = input("Enter int 'k':\n")
-k = int(k)
-
 
 def count_of_variants(x):
     m = x // 10
     n = x % 10
-    if x < 60:
-        print("It's m", m, "\nIt's n", n)
-        if n > m * 2:
-            return 0
-        elif n == m * 2 or n == m:
-            return 1
-        else:
-            if n > m:
-                n = n - m
-            combination = math.factorial(m) / (math.factorial(n) * math.factorial(m - n))
-            return combination
+    if n > m * 2:
+        return 0
+    elif n == m * 2 or n == m:
+        return 1
     else:
         if x % 10 == 0:
             sum_of_combination = 1
@@ -37,6 +27,12 @@ def count_of_variants(x):
         return sum_of_combination
 
 
-result = count_of_variants(k)
-print("You got", result, "variants")
-#review
+while True:
+    try:
+        k = int(input("Input int 'k': "))
+        result = count_of_variants(k)
+        print(result, "variants")
+    except ValueError:
+        print(" SYNTAX ERROR (k must be integer type)")
+        continue
+    break
