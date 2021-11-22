7719c0b8234efb22f618064cefcbd078135fcc99
feshovets <90150282+feshovets@users.noreply.github.com>
2021-11-22 11:24:25 +0200

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -1,3 +1,6 @@
+import os
+
+
 def print_exception(func):
     def wrapper(*args, **kwargs):
         try:
@@ -5,3 +8,19 @@ def print_exception(func):
         except Exception as error:
             print('Caught this error: ' + repr(error))
     return wrapper
+
+
+def get_valid_input(prompt="", cast=None, condition=None, error_message=None):
+    while True:
+        try:
+            res = (cast or str)(input(prompt))
+            assert condition is None or condition(res)
+            return res
+        except (ValueError, Exception):
+            print(error_message or "Error. Try Again")
+
+
+def validate_file_path(path):
+    if not os.path.exists(path):
+        raise ValueError("Non-valid file path")
+    return path
